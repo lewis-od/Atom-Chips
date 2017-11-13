16dfaf5dd00db64f4cbed320045a9372796b6e43
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2017-11-13 16:48:10 +0000

    Refactor PDE calculations into separate function

---
+++ Surface/calc_field.m
@@ -0,0 +1,79 @@
+function [phi, xq, yq, Bx, By] = calc_field(params, gm, ns, sf, edges)
+%calc_field Calculates the magnetic field given the problem params and
+%geometry
+
+mu_0 = pi*4e-7; % Permeability of free space
+sigma = params(1);
+V0 = params(2);
+d = params(3);
+z = params(4);
+
+% Set up problem geometry
+model = createpde(1);
+g = decsg(gm, sf, ns);
+geometryFromEdges(model, g);
+
+% Equation is (del)^2 phi = 0 (Laplace's equation)
+specifyCoefficients(model, 'm', 0, 'd', 0, 'c', 1, 'a', 0, 'f', 0);
+
+% Specify boundary conditions
+% Volatage of V0 on top edge
+applyBoundaryCondition(model, 'dirichlet', 'Edge', edges(1), 'r', V0);
+% Voltage of -V0 on bottom edge
+applyBoundaryCondition(model, 'dirichlet', 'Edge', edges(2), 'r', -V0);
+% Normal dervative must be 0 at all other boundaries
+applyBoundaryCondition(model, 'neumann', 'Edge', edges(3:end), 'q', 0, 'g', 0);
+
+% Generate mesh and solve PDE
+generateMesh(model);
+result = solvepde(model);
+
+% Assume geometry is given by rectangle then circles. Length of an array
+% specifying a rectangle is 10, 2nd coord says how many points are given
+n = gm(2);
+rect_x = gm(3:2+n);
+rect_y = gm(3+n:2+2*n);
+max_x = max(rect_x);
+max_y = max(rect_y);
+min_x = min(rect_x);
+min_y = min(rect_y);
+
+% Interpolate solution from mesh onto linearly spaced grid
+res = 100; % How many points per unit
+xq = linspace(min_x, max_x, (max_x-min_x)*res);
+yq = linspace(min_y, max_y, (max_y-min_y)*res);
+[xq, yq] = meshgrid(xq, yq);
+
+phi = interpolateSolution(result, xq, yq);
+phi = reshape(phi, [length(xq), length(yq)]);
+
+% Calculate electric field
+[Ex, Ey] = gradient(phi);
+Ex = -1.*Ex;
+Ey = -1.*Ey;
+
+% Remove infinities
+Ex(isnan(Ex)) = 0.0;
+Ey(isnan(Ey)) = 0.0;
+
+% Ohm's law
+jx = sigma.*Ex;
+jy = sigma.*Ey;
+
+% Calculate f(kx, ky, z, d) in Fourier space
+kx = (0:1:length(jx));
+ky = (0:1:length(jy));
+
+[kx, ky] = meshgrid(kx, ky);
+k = sqrt(kx.^2 + ky.^2);
+f_hat = (mu_0/2)*d .* ((1-exp(-d*k))./(k*d)) .* exp(-k.*z);
+
+f_hat(isnan(f_hat)) = 0.0; % Remove infinities for inverse transform
+f = real(ifft2(f_hat)); % Calculate f in real space
+
+% Calculate B using the convolution theorem
+Bx = conv2(jy, f, 'same');
+By = -conv2(jx, f, 'same');
+
+end
+
--- Surface/pde_solver.m
+++ Surface/pde_solver.m
@@ -2,15 +2,15 @@
 % Requires PDE Toolbox
 clear all;
 
-%% Parameters
+% Parameters
 sigma = 5.96e7; % Conductivity of conductor [S*m^-1]
 V0 = 1.0; % Voltage is V0 and -V0 at ends of conductor [V]
-mu_0 = pi*4e-7; % Permeability of free space
 d = 0.1; % Thickness of conductor [m]
 z = 0.5; % Distance from conductor [m]
 
-%% Specify the problem geometry
-model = createpde(1);
+params = [sigma, V0, d, z];
+
+% Specify geometry
 R1 = [3, 4, -1, 1, 1, -1, 1, 1, -1, -1];
 C1 = [1, 0, 0, 0.5]';
 C1 = [C1;zeros(length(R1) - length(C1),1)];
@@ -18,94 +18,23 @@ R1 = R1';
 gm = [R1, C1];
 sf = 'R1-C1';
 ns = char('R1', 'C1')';
-g = decsg(gm, sf, ns);
-geometryFromEdges(model, g);
+
+[phi, xq, yq, Bx, By] = calc_field(params, gm, ns, sf, [4, 2, 1, 3, 5, 6, 7, 8]);
+
+% Plot results
 figure(1);
-pdegplot(model, 'EdgeLabels', 'on');
-title('Problem Geometry', 'FontSize', 18);
-xlabel('x', 'FontSize', 16);
-ylabel('y', 'FontSize', 16);
-axis equal;
-
-%% Specify boundary conditions
-% Volatage of 1 on top edge
-applyBoundaryCondition(model, 'dirichlet', 'Edge', 4, 'r', V0);
-% Voltage of -1 on bottom edge
-applyBoundaryCondition(model, 'dirichlet', 'Edge', 2, 'r', -V0);
-% Normal dervative must be 0 at all other boundaries
-applyBoundaryCondition(model, 'neumann', 'Edge', [1,3,5,6,7,8], 'q', 0, 'g', 0);
-
-%% Specify equation coefficients
-% Equation is (del)^2 u = 0
-specifyCoefficients(model, 'm', 0, 'd', 0, 'c', 1, 'a', 0, 'f', 0);
-
-%% Generate mesh and solve equation
-generateMesh(model);
-result = solvepde(model);
-u = result.NodalSolution;
-
-%% Plot Result
-figure(2);
-plot = pdeplot(model,'XYData',u,'ZData',u);
-view(2);
-title("Solution", 'FontSize', 18);
-colormap('hsv');
-xlabel('x', 'FontSize', 16);
-ylabel('y', 'FontSize', 16);
-plot(2).Label.String = "\phi (x,y) [V]";
-plot(2).Label.FontSize = 16;
-axis equal;
-
-%% Interpolate from mesh to a square grid
-xq = linspace(-1, 1, 300);
-yq = xq;
-[xq, yq] = meshgrid(xq, yq);
-u_interp = interpolateSolution(result, xq, yq);
-u_interp = reshape(u_interp, [length(xq), length(yq)]);
-
-figure(3);
-surf(xq, yq, u_interp, 'Mesh', 'no', 'FaceColor', 'interp');
-colormap('hsv');
-view(2);
-title('Interpolated Solution', 'FontSize', 18);
+surf(xq, yq, phi, 'EdgeColor', 'none', 'FaceColor', 'interp');
+title("Electric Potential", 'FontSize', 18);
 xlabel('x', 'FontSize', 16);
 ylabel('y', 'FontSize', 16);
 c = colorbar();
-c.Label.String = "\phi (x,y) [V]";
+c.Label.String = "\phi";
 c.Label.FontSize = 16;
+colormap jet;
 axis equal;
+view(2);
 
-%% Calculate electric field
-[Ex, Ey] = gradient(u_interp);
-Ex = -1.*Ex;
-Ey = -1.*Ey;
-
-% Remove infinities
-Ex(isnan(Ex)) = 0.0;
-Ey(isnan(Ey)) = 0.0;
-
-%% Calculate low-pass filter
-% Ohm's law
-jx = sigma.*Ex;
-jy = sigma.*Ey;
-
-% Calculate f(kx, ky, z, d) in Fourier space
-kx = (0:1:length(jx));
-ky = (0:1:length(jy));
-
-[kx, ky] = meshgrid(kx, ky);
-k = sqrt(kx.^2 + ky.^2);
-f_hat = (mu_0/2)*d .* ((1-exp(-d*k))./(k*d)) .* exp(-k.*z);
-
-f_hat(isnan(f_hat)) = 0.0; % Remove infinities for inverse transform
-f = real(ifft2(f_hat)); % Calculate f in real space
-
-%% Calculate B using the convolution theorem
-Bx = conv2(jy, f, 'same');
-By = -conv2(jx, f, 'same');
-
-% Plot results
-figure(4);
+figure(2);
 B = sqrt(Bx.^2 + By.^2);
 surf(xq, yq, B, 'EdgeColor', 'none', 'FaceColor', 'interp');
 colormap('jet');
