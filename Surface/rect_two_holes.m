45a565d808afc236b38aa0afb7b1f8247d8faca8
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2017-11-14 13:33:35 +0000

    Refactor plotting into separate function

--- Surface/rect_two_holes.m
+++ Surface/rect_two_holes.m
@@ -24,26 +24,4 @@ ns = char('R1', 'C1', 'C2')';
 [phi, xq, yq, Bx, By] = calc_field(params, gm, ns, sf);
 
 % Plot results
-figure(1);
-surf(xq, yq, phi, 'EdgeColor', 'none', 'FaceColor', 'interp');
-title("Electric Potential", 'FontSize', 18);
-xlabel('x', 'FontSize', 16);
-ylabel('y', 'FontSize', 16);
-c = colorbar();
-c.Label.String = "\phi";
-c.Label.FontSize = 16;
-colormap jet;
-axis equal;
-view(2);
-
-figure(2);
-B = sqrt(Bx.^2 + By.^2);
-surf(xq, yq, B, 'EdgeColor', 'none', 'FaceColor', 'interp');
-colormap('jet');
-c = colorbar();
-c.Label.String = '|B|';
-c.Label.FontSize = 16;
-title(['B-Field for z=' num2str(z) ' and d=' num2str(d)], 'FontSize', 18);
-xlabel('x', 'FontSize', 16);
-ylabel('y', 'FontSize', 16);
-view(2);
+plot_results(phi, Bx, By, xq, yq, z, d);
