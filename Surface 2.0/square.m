bd4fa17cee8379a5fc3f3a0a4fe8e607c74f76b7
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2017-12-20 17:14:38 +0000

    No longer need PDE solved - eqn solved analytically

--- Surface 2.0/square.m
+++ Surface 2.0/square.m
@@ -10,43 +10,19 @@ V0 = 1.0; % Voltage is V0 and -V0 at ends of conductor [V]
 z = 0.5; % Distance from conductor [m]
 mu_0 = 4e-7 * pi; % Permeability of free space
 
+% Size of current elements to consider
 dx = 0.05;
 dy = 0.05;
 
-%% Set up PDE solver
-% Specify geometry
-R1 = [3, 4, -1, 1, 1, -1, 1, 1, -1, -1]';
-gm = [R1];
-sf = 'R1';
-ns = char('R1')';
-
-model = createpde(1);
-g = decsg(gm, sf, ns);
-geometryFromEdges(model, g);
-
-% Specify equation [(del)^2 phi = 0]
-specifyCoefficients(model, 'm', 0, 'd', 0, 'c', 1, 'a', 0, 'f', 0);
-
-% Specify boundary conditions
-% Volatage of V0 on top edge
-applyBoundaryCondition(model, 'dirichlet', 'Edge', 2, 'r', -V0);
-% Voltage of -V0 on bottom edge
-applyBoundaryCondition(model, 'dirichlet', 'Edge', 4, 'r', V0);
-% Normal dervative must be 0 at all other boundaries
-applyBoundaryCondition(model, 'neumann', 'Edge', [1 3], 'q', 0, 'g', 0);
-
-% Generate mesh on which to solve PDE
-generateMesh(model, 'Hmax', 0.1);
-
-%% Solve PDE
-result = solvepde(model);
+%% Analytic solution for electric potential
 res = 100; % Points per unit
 x = linspace(-1, 1, 2*res);
 y = linspace(-1, 1, 2*res);
+a = (max(x) - min(x))/2; % Boudaries at (+/-)a
+
 [x, y] = meshgrid(x, y);
 
-phi = interpolateSolution(result, x, y);
-phi = reshape(phi, size(x));
+phi = -V0/a .* x;
 
 %% Calculate current density
 [Ex, Ey] = gradient(phi);
@@ -78,7 +54,6 @@ yq = linspace(-1, 1, resolution);
 
 By = zeros(resolution, resolution);
 Bz = zeros(resolution, resolution);
-J = zeros(Ny, Nx);
 % Loop over all current elements of conductor
 for nx = 1:Nx
     for ny = 1:Ny
@@ -87,21 +62,26 @@ for nx = 1:Nx
         cx = cx - 1;
         cy = (ny-1)*dy + dy/2;
         cy = cy - 1;
-
+    
+        % Indices of current density matrix corresponding to this current
+        % element
         xN_min = (nx-1)*dxN + 1;
         xN_max = nx*dxN;
         yN_min = (ny-1)*dyN + 1;
         yN_max = ny*dyN;
-
+        
+        % Treat the segment as having uniform current density
         Jn = Jx(yN_min:yN_max, xN_min:xN_max);
         Jn = mean(mean(Jn));
-        J(ny, nx) = Jn;
+        
+        % Calculate the field due to the current segment
         [dBy, dBz] = eval_B(xq-cx, yq-cy, z, dy, dx, Jn);
         By = By + dBy;
         Bz = Bz + dBz;
     end
 end
 
+%% Plot results
 B = sqrt(By.^2 + Bz.^2);
 subplot(1, 2, 1);
 surf(xq, yq, B, 'EdgeColor', 'none');
